f0833f060f46c484ce99512b6d68b23c047b8b9e
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-11-06 18:54:10 +0000

    code refactor

--- Controllers/bank.js
+++ Controllers/bank.js
@@ -2,64 +2,84 @@ const BankModel = require("../Models/bankModel"); //imports BankModel
 const { validationResult } = require("express-validator");
 
 const listBankController = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  if (id) {
-    const bank = await BankModel.findById(id);
+    if (id) {
+      const bank = await BankModel.findById(id);
 
-    if (bank) {
-      res.json({ data: bank });
+      if (bank) {
+        res.json({ data: bank });
+      }
+    } else {
+      const banks = await BankModel.find();
+
+      if (banks) {
+        res.json({ data: banks });
+      }
     }
-  } else {
-    const banks = await BankModel.find();
-
-    if (banks) {
-      res.json({ data: banks });
-    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const createBankController = async (req, res) => {
-  const errors = validationResult(req);
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    
-    return res.json({ message: errors.array()[0].msg});
-  }
+    if (!errors.isEmpty()) {
+      return res.json({ message: errors.array()[0].msg });
+    }
 
-  const { nameOfBank, location, branch, address, tel } = req.body;
-  const newbank = new BankModel({
-    nameOfBank,
-    location,
-    branch,
-    address,
-    tel,
-  });
+    const { nameOfBank, location, branch, address, tel } = req.body;
+    const newbank = new BankModel({
+      nameOfBank,
+      location,
+      branch,
+      address,
+      tel,
+    });
 
-  const saved = await newbank.save();
+    const saved = await newbank.save();
 
-  if (saved) {
-    res.json({ message: "Bank Created", data: saved });
+    if (saved) {
+      res.json({ message: "Bank Created", data: saved });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const updateBankController = async (req, res) => {
-  const { nameOfBank, location, branch, address, tel, id } = req.body;
 
-  const update = await BankModel.updateOne(
-    { _id: id },
-    { $set: { address, tel, nameOfBank, location, branch } }
-  );
+  try {
+    const { nameOfBank, location, branch, address, tel, id } = req.body;
 
-  res.json({ message: "Bank Updated", data: update });
+    const update = await BankModel.updateOne(
+      { _id: id },
+      { $set: { address, tel, nameOfBank, location, branch } }
+    );
+
+    res.json({ message: "Bank Updated", data: update });
+    
+  } catch (error) {
+    console.log(error)
+  }
+  
 };
 
 const removeBankController = async (req, res) => {
-  const { id } = req.body;
+  try {
+      const { id } = req.body;
 
-  const removed = await BankModel.findByIdAndRemove(id);
+      const removed = await BankModel.findByIdAndRemove(id);
 
-  res.json({ message: "Bank Updated", data: removed });
+      res.json({ message: "Bank Updated", data: removed });
+    
+  } catch (error) {
+    console.log(error)
+  }
+
 };
 
 module.exports = {
--- Controllers/user.js
+++ Controllers/user.js
@@ -6,59 +6,67 @@ require("dotenv").config();
 const saltRounds = 10;
 
 const createUserController = async (req, res) => {
-  const errors = validationResult(req);
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.json({
-      message: {
-        error1: errors,
-      },
-    });
-  } else {
-    const { name, email, password } = req.body;
+    if (!errors.isEmpty()) {
+      return res.json({
+        message: {
+          error1: errors,
+        },
+      });
+    } else {
+      const { name, email, password } = req.body;
 
-    const hash = await bcrypt.hash(password, saltRounds);
+      const hash = await bcrypt.hash(password, saltRounds);
 
-    const user = new UserModel({
-      name,
-      email,
-      hash,
-    });
+      const user = new UserModel({
+        name,
+        email,
+        hash,
+      });
 
-    const saved = await user.save();
+      const saved = await user.save();
 
-    res.status(200).json({
-      message: "User Successfully created",
-      data: { name: saved.name, email: saved.email },
-    });
+      res.status(200).json({
+        message: "User Successfully created",
+        data: { name: saved.name, email: saved.email },
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const signinUserController = async (req, res) => {
-  const errors = validationResult(req);
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.json({
-      message: {
-        error1: errors,
-      },
-    });
-  } else {
-    const { email, password } = req.body;
-
-    const user = await UserModel.findOne({ email });
-    if (user) {
-      const hash = user.hash;
-      const compare = await bcrypt.compare(password, hash);
-
-      if (compare === true) {
-        res.status(200).send("User logged in Successfully");
-      } else {
-        res.status(300).send("Username of Password Invalid");
-      }
+    if (!errors.isEmpty()) {
+      return res.json({
+        message: {
+          error1: errors,
+        },
+      });
     } else {
-      res.status(300).send("User not found");
+      const { email, password } = req.body;
+
+      const user = await UserModel.findOne({ email });
+      if (user) {
+        const hash = user.hash;
+        const compare = await bcrypt.compare(password, hash);
+
+        if (compare === true) {
+          res.status(200).send("User logged in Successfully");
+        } else {
+          res.status(300).send("Username of Password Invalid");
+        }
+      } else {
+        res.status(300).send("User not found");
+      }
     }
+  } catch (error) {
+    console.log(error);
   }
 };
 
